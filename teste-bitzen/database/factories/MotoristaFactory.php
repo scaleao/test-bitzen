8a14def3111d07bed89ef78fd7c0f07fb14cb9af
João Scaleão <j.scaleao@hotmail.com>
2021-07-24 20:06:39 -0300

    feat: create Model, Controller and Migration to entity Motorista

---
+++ teste-bitzen/database/factories/MotoristaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Motorista;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MotoristaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Motorista::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
